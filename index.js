7dadac3a037be5e779196ea6e3526360667731fd
NATHULAL <nathu.ac@gmail.com>
2019-10-08 23:37:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// BlackJack
+
+let card1 = 'Ace of Spades',
+  card2 = 'Ten of Hearts';
+
+console.log('Welcome to BlackJack');
+
+console.log('You are dealt: ');
+console.log(' ' + card1);
+console.log(' ' + card2);
