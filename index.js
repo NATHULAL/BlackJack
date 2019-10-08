6785dff6de0ebc0b269d2c7ae1645a8480d7cc81
NATHULAL <nathu.ac@gmail.com>
2019-10-08 23:47:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // BlackJack
 
-let card1 = 'Ace of Spades',
-  card2 = 'Ten of Hearts';
+let deck = ["Ace of Spades", "Ten of Spades", "Three of Spades"];
+let playerCards = [deck[0], deck[1]]
 
 console.log('Welcome to BlackJack');
 
 console.log('You are dealt: ');
-console.log(' ' + card1);
-console.log(' ' + card2);
+console.log(' ' + playerCards[0]);
+console.log(' ' + playerCards[1]);
+
